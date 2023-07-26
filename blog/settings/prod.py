a86b0d842d93eb5379cf6bb9b4474b3f3075dd1a
alwaysblue930 <alwaysblue930@gmail.com>
2023-07-26 22:54:40 +0900

    housekeeping

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -1,9 +1,7 @@
 from .base import *
 
-SECRET_KEY = env('SECRET_KEY')
 DEBUG = False
 ALLOWED_HOSTS = ['*']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 import dj_database_url
 
